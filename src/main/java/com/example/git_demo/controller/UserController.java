9321e713ca694a05ef51890d42aeab1bca50d876
rrhxh <10956043+rrhxh@user.noreply.gitee.com>
2023-05-29 10:08:56 +0800

    提交branch_one 1

--- src/main/java/com/example/git_demo/controller/UserController.java
+++ src/main/java/com/example/git_demo/controller/UserController.java
@@ -10,6 +10,8 @@ public class UserController {
     public static void main(String[] args) {
 
         System.out.println("提交master 3");
+
+        System.out.println("提交branch_one 1");
     }
 
 }
